9ae0b4ccfcd56a107e05a1fc3525c8ee909beb20
Josh <bearincrypto@gmail.com>
2026-03-10 11:09:36 -0600

    bump versionCode to 5, versionName to 0.0.2-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearinmind.launcher314"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.1-beta"
+        versionCode = 5
+        versionName = "0.0.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
