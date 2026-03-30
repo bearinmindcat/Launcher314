640836b9ad58cc3f779fe5c984ff7f279df9b923
Josh <bearincrypto@gmail.com>
2026-03-29 22:19:06 -0600

    bump versionCode to 10 for v0.0.7-beta release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearinmind.launcher314"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.0.6-beta"
+        versionCode = 10
+        versionName = "0.0.7-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
