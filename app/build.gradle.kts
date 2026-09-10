6f3cbcde649da8670f1a1185fb066aecf21b6cf2
Josh <bearincrypto@gmail.com>
2026-09-10 07:55:32 -0600

    version bump to v0.0.25-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearinmind.launcher314"
         minSdk = 24
         targetSdk = 35
-        versionCode = 34
-        versionName = "0.0.24-beta_hotfix1"
+        versionCode = 35
+        versionName = "0.0.25-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
